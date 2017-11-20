e9390e7db3d419f43c4ee1cbfc1697fe04ccfedc
digitopoi <hubo85@gmail.com>
2017-11-20 15:24:13 -0600

    Create database and three models

---
+++ BankOfMurica.Data/sql/CreateAndSeedDB.sql
@@ -0,0 +1,37 @@
+﻿USE BankOfMurica;
+GO
+
+CREATE TABLE dbo.Customers
+	(
+		CustomerID int PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+		FirstName nvarchar(50) NOT NULL,
+		LastName nvarchar(50) NOT NULL,
+	);
+GO
+
+USE BankOfMurica;
+GO
+
+
+CREATE TABLE dbo.Accounts 
+	(
+		AccountID INT IDENTITY (1, 1) NOT NULL,
+		AccountNumber INT NOT NULL,
+		Balance MONEY NOT NULL,
+		Pin TINYINT NOT NULL,
+		CustomerID INT NOT NULL,
+		CONSTRAINT [Pk_dbo.AccountId] PRIMARY KEY CLUSTERED ([AccountId] ASC), 
+		CONSTRAINT [FK_dbo.Accounts_dbo.Customers_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID]) ON DELETE CASCADE
+	);
+
+CREATE TABLE dbo.Transactions
+	(TransactionID int IDENTITY (1, 1) NOT NULL,
+	AccountID int NOT NULL,
+	TransactionType nvarchar(25) NOT NULL,
+	BalanceDifference money NOT NULL,
+	TransactionDate date NOT NULL,
+
+	CONSTRAINT [PK_dbo.TransactionID] PRIMARY KEY CLUSTERED ([TransactionID] ASC),
+	CONSTRAINT [FK_dbo.Transactions_dbo.Accounts_AccoundID] FOREIGN KEY ([AccountID]) REFERENCES [dbo].[Accounts] ([AccountId]) ON DELETE CASCADE
+	);
+GO
--- BankOfMurica.Data/sql/CreateDBAddCustomersAndAccounts.sql
+++
@@ -1,5 +0,0 @@
-﻿USE BankOfMurica;
-
-INSERT dbo.Accounts (AccountNumber, Balance, Pin, CustomerID)
-	VALUES (100000000, 15000, 111, 1);
-GO
